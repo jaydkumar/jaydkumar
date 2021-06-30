af287a69de0a1529c56cbd8ba5c60f4d8ec3df5a
Jay <jay.d.kumar@gmail.com>
2021-06-30 14:25:13 -0700

    test

---
+++ IntegerToRoman.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+ var intToRoman = function(num) {
+    const map = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    }
+    
+    let result = '';
+
+    while (num > 0) {
+        for (let keys in map) {
+            if (map[keys] <= num) {
+                result += keys;
+                num -= map[keys];
+                break;
+            }
+        }
+    }
+
+    return result;
+};
+
+// TESTING
+console.log(intToRoman(68));
